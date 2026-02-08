eb733a77aae1ddbd7e0b3125fb879af82b6c0014
Code-With-Pratik <pratikkumarsoni82@gmail.com>
2026-02-08 11:59:46 +0530

    Create index.js

---
+++ src/components/notifications/index.js
@@ -0,0 +1,5 @@
+export { default as NotificationBell } from './NotificationBell';
+export { default as NotificationDropdown } from './NotificationDropdown';
+export { default as NotificationList } from './NotificationList';
+export { default as NotificationItem } from './NotificationItem';
+export { default as EmptyNotifications } from './EmptyNotifications';
