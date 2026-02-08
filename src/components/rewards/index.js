14cec1152935457dee8758353ed76648a6e24ffe
Code-With-Pratik <pratikkumarsoni82@gmail.com>
2026-02-08 11:52:10 +0530

    Create index.js

---
+++ src/components/rewards/index.js
@@ -0,0 +1,6 @@
+export { default as PointsDisplay } from './PointsDisplay';
+export { default as RewardsCard } from './RewardsCard';
+export { default as RewardsHistory } from './RewardsHistory';
+export { default as HistoryItem } from './HistoryItem';
+export { default as PointsAnimation } from './PointsAnimation';
+export { default as AchievementBadge } from './AchievementBadge';
